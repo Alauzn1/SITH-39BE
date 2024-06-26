5e8b28d4e642dd7ca24cdef9505b87171fea5d9c
Alauzn1 <52898088+Alauzn1@users.noreply.github.com>
2024-06-26 13:58:30 +0800

    Update translate.py

--- translate.py
+++ translate.py
@@ -14,22 +14,19 @@ def trans_main():
     ckpt_file = '/data/model6_6/version_0/checkpoints/epoch=399.ckpt'
     hparams_file = '/data/model6_6/version_0/hparams.yaml'
 
-    print('加载模型')
-    # 加载要使用的模型
+    print('Load model')
 
     model = Transformer_Model.load_from_checkpoint(ckpt_file)
     model.eval()
     model = model.to(device)
-    print('加载模型成功')
+    print('Successfully loaded model')
 
-    print('加载数据集')
+    print('Load Dataset')
     pl.seed_everything(0)
-    # 加载数据集
     yaml_config = yaml.load(open(hparams_file, 'r'), Loader=yaml.FullLoader)
     ds = ds_dict[yaml_config['args'].dataset]()
     ds.prepare_data()
     ds.setup()
-    print('加载数据集成功')
 
     file_path = "/home/project/Transformer_enfr/data/test_2016_flickr.en"
     output_file_path = "/home/project/Transformer_enfr/test_translate_result_enfr.txt"
