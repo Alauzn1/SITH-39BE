d47b34a911d002694188f7cc5b9bfb958b43384d
Alauzn1 <52898088+Alauzn1@users.noreply.github.com>
2024-06-26 14:13:36 +0800

    Update calculate_CIO_3D.py

--- calculate_CIO_3D.py
+++ calculate_CIO_3D.py
@@ -19,11 +19,8 @@ def centroid_distance(centroid1, centroid2):
     return np.linalg.norm(centroid1 - centroid2)
 
 def calculate_each(embedding, hidden, prefix):
-    # a 为词向量所对应的点集
-    # b 为隐含节点所对应的点集
-    # 序列长度
     seq_len = len(embedding)
-    # 去除重复行
+    # Remove duplicate rows
 
     embedding_np = np.unique(np.array(embedding), axis=0)
     hidden_np = np.unique(np.array(hidden), axis=0)
@@ -31,15 +28,15 @@ def calculate_each(embedding, hidden, prefix):
     embedding_hull = ConvexHull(embedding_np)
     hidden_hull = ConvexHull(hidden_np)
 
-    # 获取凸包的顶点
+    # Obtain the vertices of the convex hull
     embedding_vertices = embedding_hull.points[embedding_hull.vertices]
     hidden_vertices = hidden_hull.points[hidden_hull.vertices]
 
-    # 计算凸包顶点的重心
+    # Calculate the center of gravity of convex hull vertices
     embedding_centroid = np.mean(embedding_vertices, axis=0)
     hidden_centroid = np.mean(hidden_vertices, axis=0)
 
-    # 两个凸包质心的距离CIO
+    # The distance CIO between the centroids of two convex hulls
     distance = centroid_distance(embedding_centroid, hidden_centroid)
 
     return {
@@ -48,15 +45,13 @@ def calculate_each(embedding, hidden, prefix):
 
 
 def calculate(ckpt_file, hparams_file, dict_file, record_file):
-    print('开始加载并且构造字典文件, 请等待...')
-    # 加载向量文件
+    print('Starting to load and construct dictionary file, please wait...')
     print('vector_reduce = np.load dict_file', dict_file)
     vector_reduce = np.load('{}.npz'.format(dict_file))
     all_dict = {}
     for i, _ in enumerate(vector_reduce['high']):
         all_dict[generate_numpy_key(_)] = i
-    print('开始加载模型文件, 请等待...')
-    # 加载要使用的模型
+    print('Starting to load model file, please wait...')
     model = Transformer_Model.load_from_checkpoint(ckpt_file, hparams_file=hparams_file)
     model.eval()
     pl.seed_everything(0)
@@ -69,16 +64,13 @@ def calculate(ckpt_file, hparams_file, dict_file, record_file):
     with open(hparams_file, 'r') as file:
         yaml_config = yaml.load(file, Loader=yaml.FullLoader)
 
-
-    # 加载数据集
     # yaml_config = yaml.load(open(hparams_file, 'r'), Loader=yaml.FullLoader)
 
     ds = ds_dict[yaml_config['args'].dataset]()
     ds.prepare_data()
     ds.setup()
 
-
-    print('开始计算...')
+    print('Start calculation...')
     all_cal = []
 
     file_path_src = "/home/project/SITH/data/test_2016_flickr.en"
